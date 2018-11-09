23a3edad2a41a9b45b147d6cd562d4d64523bd5d
Jan Jaeger <j.jaeger@eos-did.com>
2018-11-09 16:20:05 +0100

    added Status bar message on HTTP thread hang.

--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, httpasyncthread, HTTPDefs, DateUtils, IniFiles;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, ComCtrls, httpasyncthread, HTTPDefs, DateUtils, IniFiles;
 
 
 type
@@ -17,8 +18,11 @@ type
     Label1: TLabel;
     Memo1: TMemo;
     Memo2: TMemo;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
     procedure CopyRequest(var request: string; var user, pass: string);
     procedure CopyResponse(response: string; cookies: TStrings);
+    procedure CheckResponse(Sender: TObject);
 
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -29,6 +33,9 @@ type
     _request: string;
     _response: string;
     _authNamespace: string;
+    requestReceived: boolean;
+    timeAtLastRequest: TDateTime;
+    waitFor: integer;
 
   public
 
@@ -61,6 +68,23 @@ procedure TForm1.CopyResponse(response: string; cookies: TStrings);
 begin
     // pass data from http thread back to main thread
     Memo1.Text:= response;
+    requestReceived:= True;
+end;
+
+procedure TForm1.CheckResponse(Sender: TObject);
+begin
+    if (requestReceived) then
+    begin
+      StatusBar1.SimpleText:= '';
+      Timer1.Enabled:= False;
+    end else
+    begin
+      if (MilliSecondsBetween(timeAtLastRequest, Now) > waitFor) then
+      begin
+        StatusBar1.SimpleText:= 'Waiting for server response...';
+        Timer1.Enabled:= True;
+      end;
+    end;
 end;
 
 //*******************************
@@ -69,7 +93,11 @@ procedure TForm1.Button1Click(Sender: TObject);
 begin
     if (assigned(myHTTPAsyncThread)) then
        Memo2.Text:= myHTTPAsyncThread.ToString;
-
+    timeAtLastRequest:= Now;
+    requestReceived:= False;
+    waitFor:= 1000;
+    Timer1.Enabled:= True;
+    Timer1.OnTimer:= @CheckResponse;
     myHTTPAsyncThread.makeHTTPRequest;
 end;
 
@@ -88,6 +116,7 @@ begin
     THTTPAsyncThread.CreateOrRecycle(myHTTPAsyncThread);
     myHTTPAsyncThread.OnSyncRequestParams:= @CopyRequest;
     myHTTPAsyncThread.OnSynchResponseData:= @CopyResponse;
+    Timer1.Enabled:= False;
 end;
 
 
